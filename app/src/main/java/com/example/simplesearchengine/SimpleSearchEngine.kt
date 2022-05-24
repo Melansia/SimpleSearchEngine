7674ba90ed3619f9c24b623de558e45a9aa2c2d2
Greg <lupasco.grigori@gmail.com>
2022-05-24 19:11:09 +0300

    SimpleSearchEngine.kt

--- app/src/main/java/com/example/simplesearchengine/SimpleSearchEngine.kt
+++ app/src/main/java/com/example/simplesearchengine/SimpleSearchEngine.kt
@@ -1,12 +1,46 @@
 package com.example.simplesearchengine
 
-fun main() {
-    val inputString = readln().split(" ")
-    val orderOfWord = readln()
+class SimpleSearchEngine {
 
-    when {
-        inputString.contains(orderOfWord) -> println(inputString.indexOf(orderOfWord) + 1)
-        else -> println("Not Found")
+    private val searchResult = mutableListOf<String?>()
+    private lateinit var dataBase: MutableList<String>
+
+    fun start() {
+        println("Enter the number of people:")
+        val size = readln().toInt()
+        println("Enter all people: ")
+        dataBase = MutableList(size) { readln() }
+
+        println()
+        println("Enter the number of search queries:")
+        searchForResult(readln().toInt())
     }
 
+    private fun searchForResult(numOfQueries: Int) {
+        repeat(numOfQueries) {
+            println()
+            println("Enter data to search people:")
+            val dataEntered = readln()
+            for (i in dataBase) {
+                when {
+                    i.contains(dataEntered, true) -> searchResult.add(i)
+                    else -> continue
+                }
+            }
+            if (searchResult.isNotEmpty()) {
+                println()
+                println("People found:")
+                searchResult.forEach {
+                    println(it)
+                }
+            } else {
+                println("No matching people found.")
+            }
+            searchResult.clear()
+        }
+    }
+}
+
+fun main() {
+    SimpleSearchEngine().start()
 }
